24c92edb2cdbb4a957b20bea1decb02cf63d3fbd
WeersProductions <weersproductions@gmail.com>
2016-12-22 13:46:00 +0100

    Extracted to own project.

---
+++ src/ss/project/shared/UIController.java
@@ -0,0 +1,15 @@
+package ss.project.shared;
+
+
+import ss.project.shared.game.World;
+
+public class UIController {
+
+    public UIController() {
+
+    }
+
+    public void Show(World world) {
+
+    }
+}
---
+++ src/ss/project/shared/ui/ServerTUI.java
@@ -0,0 +1,19 @@
+package ss.project.shared.ui;
+
+import ss.project.shared.IStartStop;
+
+/**
+ * Created by simon on 09.12.16.
+ */
+public class ServerTUI extends ServerUI implements IUserInterface, IStartStop {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
