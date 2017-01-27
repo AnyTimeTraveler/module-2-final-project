5812802eb32fea7ddc7ae8f5de9a759ba2beed6e
Simon Struck <simonstruck70@gmail.com>
2017-01-27 23:52:41 +0100

    Created ChatMessageTest

--- src/ss/project/shared/ChatMessage.java
+++ src/ss/project/shared/ChatMessage.java
@@ -1,28 +1,27 @@
 package ss.project.shared;
 
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Setter;
 
 /**
  * Created by simon on 26.01.17.
  */
 @Data
 public class ChatMessage {
-    @Setter(AccessLevel.PRIVATE)
-    private long date;
+    private String name;
     private String message;
 
-    public ChatMessage(String message) {
-        setDate(System.currentTimeMillis());
+    public ChatMessage(String name, String message) {
+        setName(name);
         setMessage(message);
     }
 
     public static ChatMessage fromString(String line) {
-        if (Protocol.Server.NOTIFYMESSAGE.equals(line.substring(0, line.indexOf(' ')))) {
-            return new ChatMessage(line.substring(line.indexOf(' ') + 1));
-        } else {
-            return new ChatMessage(line);
-        }
+        int nameStart = line.indexOf(' ') + 1;
+        int messageStart = line.indexOf(' ', nameStart);
+        return new ChatMessage(line.substring(nameStart, messageStart), line.substring(messageStart + 1));
+    }
+
+    public String serialize() {
+        return name + " " + message;
     }
 }
---
+++ tests/ChatmessageTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+import ss.project.shared.ChatMessage;
+import ss.project.shared.Protocol;
+
+/**
+ * Created by simon on 27.01.17.
+ */
+public class ChatmessageTest {
+    @Test
+    public void fromString() {
+        String name = "Simon";
+        String message = "Hello, this is a Test.";
+        String serializedMessage = Protocol.createMessage(Protocol.Server.NOTIFYMESSAGE, name, message);
+        ChatMessage chatMessage = ChatMessage.fromString(serializedMessage);
+        Assert.assertEquals(name, chatMessage.getName());
+        Assert.assertEquals(message, chatMessage.getMessage());
+        Assert.assertEquals(new ChatMessage(name, message), chatMessage);
+    }
+
+    @Test
+    public void serialize() {
+        String name = "Simon";
+        String message = "Hello, this is a Test.";
+        ChatMessage chatMessage = new ChatMessage(name, message);
+        ChatMessage chatMessage1 = ChatMessage.fromString(Protocol.createMessage(Protocol.Server.NOTIFYMESSAGE, chatMessage));
+
+        Assert.assertEquals(chatMessage, chatMessage1);
+    }
+}
